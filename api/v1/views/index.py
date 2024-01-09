a962d4632b452126e8be0ef72bf193d1a0bb4953
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-01-09 20:58:07 +0300

    AirBnB_clone

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python3
 """Creates a route on app_views"""
-from flask import jsonify
+from flask import jsonify, Flask, Blueprint
 from api.v1.views import app_views
+from models import storage
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """Returns a JSON response for status"""
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
+
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """hbnbStatus"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """hbnbStats"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
+
+if __name__ == "__main__":
+    pass
